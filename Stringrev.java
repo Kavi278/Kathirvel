1cec5e2d6951d05adc6051094bc7c988175554aa
Kavi278 <kathir.nd@gmail.com>
2016-08-30 15:33:05 +0530

    Stringrev.java

---
+++ Stringrev.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Stringrev
+{
+        public static void main(String args[])
+        {
+            Scanner in = new Scanner(System.in);
+            String s = in.next();
+            StringBuffer sb = new StringBuffer(s);
+            System.out.println(sb.reverse());
+        }
+  }
