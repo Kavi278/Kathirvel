c62436003b41ce7a400d2c59bdca02234ba3e0aa
Kavi278 <kathir.nd@gmail.com>
2016-08-30 15:57:51 +0530

    Strto.java

---
+++ Strto.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Strto
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		try
+		{
+			System.out.println(Integer.parseInt(s));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
